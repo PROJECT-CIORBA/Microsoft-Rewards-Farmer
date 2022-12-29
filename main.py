32226088ab53a7756ec259c00c73aa54f73899ed
RealBandIasi <realbandiasi@yahoo.com>
2022-12-29 17:59:12 +0200

    Fixed a mild bug

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ a = 0
 
 
 subprocess.call(['C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'])
-while a <= 10:
+while a <= 30:
     raw_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     list = random.choice(raw_list)
     time.sleep(1.5)
@@ -19,4 +19,4 @@ while a <= 10:
     pyautogui.hotkey('ctrl', 't')
     pyautogui.hotkey('ctrl', '1')
     pyautogui.hotkey('ctrl', 'w')
-    a += 1
+    a += 3
