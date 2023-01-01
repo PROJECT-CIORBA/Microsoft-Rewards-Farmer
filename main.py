85c20bc27c4adf045c22a68c0e643a4b7baf3a50
PROJECT-CIORBA <steffiilas@gmail.com>
2023-01-01 13:05:45 +0200

    Added comments.

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ import time
 a = 0
 
 
-
+#Opens Microsoft Edge, for obvious reasons.
 subprocess.call(['C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'])
+#Searchs random letters 10 times.
 while a <= 10:
+    #Probably could've been written more efficiently.
     raw_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     list = random.choice(raw_list)
     time.sleep(1)
     pyautogui.write(list)
     pyautogui.press('enter')
     time.sleep(1.5)
+    #Browser shortcuts, my favourite.
     pyautogui.hotkey('ctrl', 't')
     pyautogui.hotkey('ctrl', '1')
     pyautogui.hotkey('ctrl', 'w')
     a += 1
 b = 0
+#Uses Bing (;-;), and does the same thing.
 while b <= 30:
+    #Opens the url.
+    #Again, could've been written more efficiently.
     pyautogui.hotkey("ctrl", "t")
     pyautogui.write("https://www.bing.com/news/?form=ml11z9&crea=ml11z9&wt.mc_id=ml11z9&rnoreward=1&rnoreward=1")
     pyautogui.press("enter")
@@ -36,6 +42,7 @@ while b <= 30:
     time.sleep(0.5)
     pyautogui.press('tab')
     time.sleep(1)
+    #Same stuff as last time.
     raw_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     list = random.choice(raw_list)
     pyautogui.write(list)
@@ -45,4 +52,5 @@ while b <= 30:
     pyautogui.hotkey('ctrl', '1')
     pyautogui.hotkey('ctrl', 'w')
     b += 3
-pyautogui.hotkey('alt', 'f4')
+#Closes the last tab, making the browser close.
+pyautogui.hotkey('ctrl', 'w')
