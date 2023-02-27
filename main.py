35104aa7737826bd2ee364700efa8b6af03c53c8
PROJECT-CIORBA <steffiilas@gmail.com>
2023-02-27 08:16:40 +0200

    Updated script to work with the new bing site

--- main.py
+++ main.py
@@ -52,9 +52,7 @@ if level == "1":
         pyautogui.press("tab")
         time.sleep(0.5)
         pyautogui.press('tab')
-        time.sleep(0.5)
-        pyautogui.press('tab')
-        time.sleep(1)
+
         #Same stuff as last time.
         raw_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', "1", "2", "3","4","5","6","7","8","9","0"]
         ran_num = random.randrange(0,10)
@@ -90,8 +88,6 @@ elif level == "2":
         pyautogui.hotkey('ctrl', 'w')
         time.sleep(1)
         pyautogui.press("tab")
-        time.sleep(0.5)
-        pyautogui.press('tab')
         time.sleep(0.5)
         pyautogui.press('tab')
         time.sleep(1)
